602a2527ae41eb964b2d90c0eb72ef83019dc32e
AlehKhvasko <alehkhvasko@yahoo.com>
2022-01-17 22:01:36 -0600

    third commit

--- Box.java
+++ Box.java
@@ -3,6 +3,7 @@ package projectsHandsOn.generics.BoxOfFruits;
 import java.util.ArrayList;
 
 public class Box <T extends Fruit>  implements Comparable<Box<?>>  {
+
     private ArrayList<T> fruits = new ArrayList<>();
     private double totalWeight;
 
@@ -30,15 +31,27 @@ public class Box <T extends Fruit>  implements Comparable<Box<?>>  {
         catch (EmptyWeightException e){
             System.out.println(e);
         }
-
         totalWeight += fruit.getWeight();
-
     }
+
+    /**
+     * not finished
+     * Question how to create dynamic types of objects in runtime AKA polymorphism.
+     * @param
+     */
+  /*  public void toEmptyBox(Box<T> box){
+        for (int i = 0; i < box.getNumberOfFruits(); i++) {
+            box.addFruit((T) new <Apple>());
+            fruits.remove(fruits.size());
+        }
+    }*/
 
     public double getWeight(){
         return totalWeight;
     }
-
+    /**
+     * useless lines of code?
+     */
     public void compareBoxes(Box<?> box){
         int result = compareTo(box);
         if (result > 0){
